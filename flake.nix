68f5e6b8f4c1035f37ced3df63645c242495adc4
wochap <gean.marroquin@gmail.com>
2026-08-13 19:23:51 -0500

    match nixos nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "kb-hud development shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=0ad6f47ea4fe188f4bc8f0380f93ae8523337c6c"; # nixos-26.05 (10 jul 2026)
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
